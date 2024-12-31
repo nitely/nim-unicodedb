67cc549520fa09045dc47e7a979ded7211005a4b
Esteban C Borsani <ecastroborsani@gmail.com>
2024-12-30 22:30:38 -0300

    resolveCaseFold (#36)

--- src/unicodedb/casing.nim
+++ src/unicodedb/casing.nim
@@ -5,17 +5,19 @@ import std/unicode
 
 import ./casing_data
 
+template ones(n: untyped): uint32 = (1.uint32 shl n) - 1
+
 # This jumps through hoops to
 # contain a single yield
 template casingImpl(
-  offsets, indices, data, blockSize: untyped
-): untyped {.dirty.} =
+  r, offsets, indices, data, blockSize: untyped
+): untyped =
   let blockOffset = (offsets[r.int div blockSize]).int * blockSize
   var idx = (indices[blockOffset + r.int mod blockSize]).int
-  # The len is encoded into the 2 least 
+  # The len is encoded into the 3 least 
   # significant bits of the first cp
-  let idxEnd = if idx > -1: idx+(data[idx] and 0x03).int-1 else: -1
-  var rr = if idx > -1: cast[int32](data[idx] shr 2).Rune else: r
+  let idxEnd = if idx > -1: idx+(data[idx] and ones(3)).int-1 else: -1
+  var rr = if idx > -1: cast[int32](data[idx] shr 3).Rune else: r
   while true:
     yield rr
     inc idx
@@ -28,30 +30,36 @@ iterator lowerCase*(r: Rune): Rune {.inline.} =
   ## there is such mapping. Return `r` otherwise
   doAssert r.int <= 0x10FFFF
   casingImpl(
+    r,
     lowercaseOffsets,
     lowercaseIndices,
     lowercaseData,
-    lowercaseBlockSize)
+    lowercaseBlockSize
+  )
 
 iterator upperCase*(r: Rune): Rune {.inline.} =
   ## Return upper case mapping of `r` if
   ## there is such mapping. Return `r` otherwise
   doAssert r.int <= 0x10FFFF
   casingImpl(
+    r,
     uppercaseOffsets,
     uppercaseIndices,
     uppercaseData,
-    uppercaseBlockSize)
+    uppercaseBlockSize
+  )
 
 iterator titleCase*(r: Rune): Rune {.inline.} =
   ## Return title case mapping of `r` if
   ## there is such mapping. Return `r` otherwise
   doAssert r.int <= 0x10FFFF
   casingImpl(
+    r,
     titlecaseOffsets,
     titlecaseIndices,
     titlecaseData,
-    titlecaseBlockSize)
+    titlecaseBlockSize
+  )
 
 iterator caseFold*(r: Rune): Rune {.inline.} =
   ## Return full case fold of `r` if
@@ -60,10 +68,12 @@ iterator caseFold*(r: Rune): Rune {.inline.} =
   ## text comparison
   doAssert r.int <= 0x10FFFF
   casingImpl(
+    r,
     casefoldOffsets,
     casefoldIndices,
     casefoldData,
-    casefoldBlockSize)
+    casefoldBlockSize
+  )
 
 func simpleCaseFold*(r: Rune): Rune =
   ## Return simple case fold of `r` if
@@ -81,3 +91,16 @@ func hasCaseFolds*(r: Rune): bool =
   doAssert r.int <= 0x10FFFF
   let blockOffset = (hasCasefoldsIndices[r.int div hasCasefoldsBlockSize]).int * hasCasefoldsBlockSize
   result = hasCasefoldsData[blockOffset + r.int mod hasCasefoldsBlockSize] == 1
+
+iterator resolveCaseFold*(r: Rune): Rune {.inline.} =
+  ## Return all code points that have the same
+  ## simple case-fold map of `r`. It always includes `r`.
+  ## Uses simple case folding.
+  doAssert r.int <= 0x10FFFF
+  casingImpl(
+    r.simpleCaseFold(),
+    resolveCasefoldOffsets,
+    resolveCasefoldIndices,
+    resolveCasefoldData,
+    resolveCasefoldBlockSize
+  )
